d392b18ee55e17736df9fffe1f9693c6de3a32fe
Will Kruse <wskruse@gmail.com>
2019-02-08 13:03:00 -0500

    Add an authGuard to prevent access to routes anonymously

--- src/router.js
+++ src/router.js
@@ -3,25 +3,37 @@ import Router from 'vue-router';
 import Library from './views/Library.vue';
 import Reports from './views/Reports.vue';
 import Login from './views/Login.vue';
+import store from './store';
 
 Vue.use(Router);
 
-export default new Router({
-  routes: [
-    {
-      path: '/library',
-      name: 'library',
-      component: Library,
-    },
-    {
-      path: '/reports',
-      name: 'reports',
-      component: Reports,
-    },
-    {
-      path: '/',
-      name: 'login',
-      component: Login,
-    },
+const router = new Router({
+  mode: 'history',
+  routes: [{
+    path: '/',
+    name: 'library',
+    component: Library,
+    alias: '/library',
+  },
+  {
+    path: '/reports',
+    name: 'reports',
+    component: Reports,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+  },
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  if (!store.state.authenticated && to.name !== 'login') {
+    next('/login');
+  } else {
+    next();
+  }
+});
+
+export default router;
