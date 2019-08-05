f21849f7b7e3098cf303badcf679e6e4881cfe02
nglushien17 <nglushien@ushmm.org>
2019-08-05 15:30:38 -0400

    set authenticated to false

--- src/store.js
+++ src/store.js
@@ -1,18 +1,16 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from "vue";
+import Vuex from "vuex";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    authenticated: true,
+    authenticated: false
   },
   mutations: {
     setAuthenticated(state, authState) {
-      Vue.set(state, 'authenticated', authState);
-    },
+      Vue.set(state, "authenticated", authState);
+    }
   },
-  actions: {
-
-  },
+  actions: {}
 });
