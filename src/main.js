7de1f012af09f8119238287cb57c7d66c8435fd5
nglushien17 <nglushien@ushmm.org>
2019-08-05 09:23:03 -0400

    updated the style of the vuefire import

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-import Vue from 'vue';
-import './plugins/vuetify';
-import VueFire from 'vuefire';
-import firebase from 'firebase/app';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import 'firebase/firestore';
-import 'firebase/auth';
-import env from '../env.json';
+import Vue from "vue";
+import "./plugins/vuetify";
+import { firestorePlugin } from "vuefire";
+import firebase from "firebase/app";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "firebase/firestore";
+import "firebase/auth";
+import env from "../env.json";
 
 Vue.config.productionTip = false;
 
-Vue.use(VueFire);
-Vue.use(require('vue-moment'));
+Vue.use(firestorePlugin);
+Vue.use(require("vue-moment"));
 
 firebase.initializeApp(env.firebase);
 
@@ -24,5 +24,5 @@ Vue.prototype.$auth = auth;
 new Vue({
   router,
   store,
-  render: h => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount("#app");
