c2778ce0c5f217bf5975a28d29aa81d962a35fd6
nglushien17 <nglushien@ushmm.org>
2019-08-09 12:08:38 -0400

    removed unnecessary row

--- src/views/Reports.vue
+++ src/views/Reports.vue
@@ -1,10 +1,5 @@
 <template>
   <v-container grid-list-md text-xs-center>
-    <!-- <v-layout row wrap>
-      <v-flex xs12>
-        <h1 class="text-xs-left font-weight-regular">Reports</h1>
-      </v-flex>
-    </v-layout> -->
     <v-layout row wrap>
       <ReportCards />
     </v-layout>
@@ -12,11 +7,11 @@
 </template>
 
 <script>
-import ReportCards from '../components/ReportCards.vue';
+import ReportCards from "../components/ReportCards.vue";
 
 export default {
   components: {
-    ReportCards,
-  },
+    ReportCards
+  }
 };
 </script>
