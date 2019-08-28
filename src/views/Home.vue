77387947583155f74863d2e9040a8b13a8182d50
nglushien17 <nglushien@ushmm.org>
2019-08-28 10:09:01 -0400

    changed the text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,10 +8,10 @@
         <div class="v-responsive__content">
           <div class="container">
             <div class="layout align-center justify-center wrap">
-              <div class="text-center" style="flex:0 1 auto;">
-                <h1 class="font-weight-light mb-6 mx-4 display-3">
-                  Books
-                  <br />In Our Collection
+              <div class="text-left text-md-center" style="flex:0 1 auto;">
+                <h1 class="font-weight-light mb-6 mx-4 custom-headline">
+                  Go Ahead...
+                  <br />Start Keeping Track of Your Books
                 </h1>
               </div>
             </div>
@@ -34,15 +34,11 @@
                         <div class="v-responsive__sizer" style="padding-bottom:38.46153846153846%;"></div>
                         <div
                           class="v-image__image v-image__image--cover"
-                          style="background-image: url(&quot;https://cdn.vuetifyjs.com/images/home/feature3.png&quot;); background-position: center center;"
+                          style="background-image: url(&quot;https://placeimg.com/668/402/tech/grayscale&quot;); background-position: center center;"
                         ></div>
-                        <div class="v-responsive__content"></div>
                       </div>
                       <div class="v-card__text">
-                        <h2 class="subtitle-1 font-weight-bold mb-2 text--primary">Vibrant Community</h2>
-                        <p
-                          class="mb-2"
-                        >When you run into a roadblock, you need assistance right away. Vuetify offers support in our massive community on Discord.</p>
+                        <p class="mb-2">Review a collection of the books we own.</p>
                       </div>
                     </div>
                   </div>
@@ -60,17 +56,12 @@
                         <div class="v-responsive__sizer" style="padding-bottom:38.46153846153846%;"></div>
                         <div
                           class="v-image__image v-image__image--cover"
-                          style="background-image: url(&quot;https://cdn.vuetifyjs.com/images/home/feature2.png&quot;); background-position: center center;"
+                          style="background-image: url(&quot;https://placeimg.com/668/402/nature/grayscale&quot;); background-position: center center;"
                         ></div>
                         <div class="v-responsive__content"></div>
                       </div>
                       <div class="v-card__text">
-                        <h2
-                          class="subtitle-1 font-weight-bold mb-2 text--primary"
-                        >Semantic Material Components</h2>
-                        <p
-                          class="mb-2"
-                        >Be prepared for an armada of specialized components at your disposal. With over 80 in total, there is a solution to any situation.</p>
+                        <p class="mb-2">Analyize the data from your library.</p>
                       </div>
                     </div>
                   </div>
@@ -88,17 +79,12 @@
                         <div class="v-responsive__sizer" style="padding-bottom:38.46153846153846%;"></div>
                         <div
                           class="v-image__image v-image__image--cover"
-                          style="background-image: url(&quot;https://cdn.vuetifyjs.com/images/home/feature1.png&quot;); background-position: center center;"
+                          style="background-image: url(&quot;https://placeimg.com/668/402/arch/grayscale&quot;); background-position: center center;"
                         ></div>
                         <div class="v-responsive__content"></div>
                       </div>
                       <div class="v-card__text">
-                        <h2
-                          class="subtitle-1 font-weight-bold mb-2 text--primary"
-                        >Ready-Made Project Scaffolding</h2>
-                        <p
-                          class="mb-2"
-                        >Vuetify supports the future of Vue tooling through its Vue CLI 3 plugin. With one command you are ready to start building your next great idea.</p>
+                        <p class="mb-2">Manage the entire library from one interface.</p>
                       </div>
                     </div>
                   </div>
@@ -117,3 +103,14 @@ export default {
   name: "home"
 };
 </script>
+<style lang="scss" scoped>
+@media (min-width: 768px) {
+  h1 {
+    font-size: 3.75rem !important;
+    font-weight: 300;
+    line-height: 4.5rem;
+    letter-spacing: -0.0083333333em !important;
+    font-family: "Roboto", sans-serif !important;
+  }
+}
+</style>
