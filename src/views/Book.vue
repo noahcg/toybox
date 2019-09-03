f622bbb87764c5c6a3a842502b33d47e8992af53
nglushien17 <nglushien@ushmm.org>
2019-09-03 10:52:38 -0400

    added a conditional loading overlay

--- src/views/Book.vue
+++ src/views/Book.vue
@@ -3,7 +3,10 @@
     <div class="pa-12">
       <section class="layout wrap mb-12">
         <div class="flex">
-          <div class="container grey--text text--darken-2 container--fluid grid-list-md pa-0">
+          <div
+            v-if="!overlay"
+            class="container grey--text text--darken-2 container--fluid grid-list-md pa-0 book-content"
+          >
             <p class="title">{{ book.title }}</p>
             <p>Author: {{ book.author }}</p>
             <p>Category: {{ book.category }}</p>
@@ -14,6 +17,9 @@
         </div>
       </section>
     </div>
+    <v-overlay :value="overlay">
+      <v-progress-circular indeterminate size="64"></v-progress-circular>
+    </v-overlay>
   </v-container>
 </template>
 
@@ -22,8 +28,18 @@ import { db } from "../main";
 export default {
   name: "book",
   data: () => ({
-    book: []
+    book: [],
+    overlay: true
   }),
+  watch: {
+    book() {
+      if (this.book.length < 1) {
+        this.overlay = true;
+      } else {
+        this.overlay = false;
+      }
+    }
+  },
   firestore() {
     return {
       book: db.collection("books").doc(this.$route.params.id)
